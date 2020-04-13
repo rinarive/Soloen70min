a467be8075703c490429ecae53d1efa4ee04ba91
Rena <rinarive@gmail.com>
2020-04-13 18:52:23 -0400

    ordenando folders

---
+++ build/precache-manifest.949e3a85eaa47f9ae3d1dcb345354973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39c47506e769bb277020f5bbb4982f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3250f32b47e6545a952f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a87b34bced631210b6bf",
+    "url": "/static/js/2.66f5ba3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66f5ba3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3250f32b47e6545a952f",
+    "url": "/static/js/main.988b2c43.chunk.js"
+  },
+  {
+    "revision": "0f50091cc05e12622e86",
+    "url": "/static/js/runtime-main.3c1a19f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
