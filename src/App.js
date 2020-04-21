d52874d4659177e3a672aa37a7352bc0f9be1401
rinarive <rinarive@gmail.com>
2020-04-21 10:38:17 -0400

    agrego redux

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Home from './pages/Home';
+import Results from './pages/Results';
+
+
+const App = ({ store }) => (
+	<Provider store={store}>
+		<Router>
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route path = "/results" component={Results}/>
+			</div>
+		</Router>
+	</Provider>
+);
+
+App.propTypes = {
+	store: PropTypes.object.isRequired
+};
+
+export default App;
